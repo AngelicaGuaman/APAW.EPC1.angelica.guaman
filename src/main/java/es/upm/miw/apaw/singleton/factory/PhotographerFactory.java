d78a60817feb0d9d919991e61a425123e08f0a40
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-12 22:09:56 +0200

    added singleton

---
+++ src/main/java/es/upm/miw/apaw/singleton/factory/PhotographerFactory.java
@@ -0,0 +1,42 @@
+package es.upm.miw.apaw.singleton.factory;
+
+import es.upm.miw.apaw.entities.Photographer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhotographerFactory {
+    private Map<String, Photographer> photographers;
+
+    private static final PhotographerFactory factory = new PhotographerFactory();
+
+    private PhotographerFactory() {
+        this.photographers = new HashMap<>();
+    }
+
+    public Photographer getPhotographer(String key) {
+        for (Map.Entry<String, Photographer> entry : photographers.entrySet()) {
+            if (entry.getKey().equals(key)) {
+                return this.photographers.get(key);
+            }
+        }
+
+        return null;
+    }
+
+    public void addPhotographer(Photographer photographer) {
+        this.photographers.put(photographer.getId(), photographer);
+    }
+
+    public void removePhotographer(String key) {
+        this.photographers.remove(key);
+    }
+
+    public int getPhotographerFactorySize() {
+        return this.photographers.size();
+    }
+
+    public static PhotographerFactory getFactory() {
+        return factory;
+    }
+}
