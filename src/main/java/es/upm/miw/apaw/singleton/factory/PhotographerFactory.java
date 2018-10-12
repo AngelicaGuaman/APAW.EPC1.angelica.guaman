a9c8bcc563871faf1ac7f88fcf6bb8c42b446603
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-12 23:27:51 +0200

    modified singleton factory

--- src/main/java/es/upm/miw/apaw/singleton/factory/PhotographerFactory.java
+++ src/main/java/es/upm/miw/apaw/singleton/factory/PhotographerFactory.java
@@ -1,11 +1,12 @@
 package es.upm.miw.apaw.singleton.factory;
 
 import es.upm.miw.apaw.entities.Photographer;
+import es.upm.miw.apaw.observer.Observable;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class PhotographerFactory {
+public class PhotographerFactory extends Observable {
     private Map<String, Photographer> photographers;
 
     private static final PhotographerFactory factory = new PhotographerFactory();
@@ -26,10 +27,12 @@ public class PhotographerFactory {
 
     public void addPhotographer(Photographer photographer) {
         this.photographers.put(photographer.getId(), photographer);
+        this.notifyObservers();
     }
 
     public void removePhotographer(String key) {
         this.photographers.remove(key);
+        this.notifyObservers();
     }
 
     public int getPhotographerFactorySize() {
